9b531829099042035b3eaa8bd61ec1aec2e1ba58
Khánh Duy <150520249+DuyNTK3020@users.noreply.github.com>
2025-07-31 22:07:30 +0700

    Move and update khanhduy.js and index.html files
    Deleted js-ts/khanhduy.js and added variable/khanhduy.js with expanded variable usage examples. Renamed js-ts/index.html to variable/index.html for better organization.

---
+++ variable/khanhduy.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+// Phạm vi sử dụng biến rộng rãi. Dễ sinh ra bug
+var name = 'khanh duy';
+
+const name1 = 'thanduyly';
+
+// Kiểm soát phạm vi nhỏ hơn. Chỉ trong phạm vi của 1 {}
+let name2 = 'hihi';
+
+let age = 20;
+age = 30;
+
+// Là hằng số
+const myAge = 25;
+
+console.log(age);
