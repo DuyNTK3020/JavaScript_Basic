9853d253b45e7a302bf30cbc3b0f7cb19f4dcddd
Khánh Duy <150520249+DuyNTK3020@users.noreply.github.com>
2025-08-02 16:32:36 +0700

    Add control flow example with HTML and JS
    Introduced a new control_flow directory containing an index.html and a script.js file. The HTML file loads the script, which demonstrates basic control flow in JavaScript by logging a message based on a score value.

---
+++ control_flow/script.js
@@ -0,0 +1,14 @@
+const score = 4.0;
+
+if (score >= 3.6) {
+    console.log("Xuat sac");
+}
+else if (score >= 3.2) {
+    console.log("Gioi");
+}
+else if (score >= 2.8) {
+    console.log("Kha");
+}
+else {
+    console.log("Tach");
+}
