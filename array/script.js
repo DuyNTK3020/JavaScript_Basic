521293bc6216d38c60fbfc7568394106436cd7bc
Khánh Duy <150520249+DuyNTK3020@users.noreply.github.com>
2025-08-05 22:27:35 +0700

    Add array manipulation example with HTML and JS
    Created index.html and script.js demonstrating basic array operations in JavaScript, including accessing, modifying, and using push, unshift, pop, and shift methods. The HTML file loads the script and displays a heading.

---
+++ array/script.js
@@ -0,0 +1,21 @@
+let colors = ["red", "green", "blue"];
+
+console.log(colors[0]);
+
+colors[0] = "yellow"
+
+console.log(colors[0]);
+
+console.log(colors);
+
+colors.push("orange");
+console.log(colors);
+
+colors.unshift("test");
+console.log(colors);
+
+colors.pop();
+console.log(colors);
+
+colors.shift()
+console.log(colors);
